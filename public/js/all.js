eefd2d35c941530cf5d78c3a02ac4f466e1d964c
dinwoo <danielforboy@gmail.com>
2021-10-12 23:59:52 +0800

    first commit

---
+++ public/js/all.js
@@ -0,0 +1,15 @@
+"use strict";
+
+var fun = function fun() {
+  console.log("!!!!");
+};
+
+fun();
+"use strict";
+
+var fun2 = function fun2() {
+  console.log("2");
+};
+
+fun2();
+//# sourceMappingURL=all.js.map
